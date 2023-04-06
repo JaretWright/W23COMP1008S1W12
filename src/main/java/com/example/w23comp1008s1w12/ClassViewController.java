1f94e0ea086e170b70ad612724b77f0a7685f87b
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-04-06 17:27:33 -0400

    Initial commit

---
+++ src/main/java/com/example/w23comp1008s1w12/ClassViewController.java
@@ -0,0 +1,29 @@
+package com.example.w23comp1008s1w12;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClassViewController implements Initializable {
+
+    @FXML
+    private Label ageLabel;
+
+    @FXML
+    private ListView<?> listView;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label stNumLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ src/main/java/com/example/w23comp1008s1w12/Student.java
@@ -0,0 +1,39 @@
+package com.example.w23comp1008s1w12;
+
+import java.time.LocalDate;
+
+public class Student {
+    private String name;
+    private int studentNum;
+    private LocalDate birthday;
+
+    public Student(String name, int studentNum, LocalDate birthday) {
+        this.name = name;
+        this.studentNum = studentNum;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStudentNum() {
+        return studentNum;
+    }
+
+    public void setStudentNum(int studentNum) {
+        this.studentNum = studentNum;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+}
