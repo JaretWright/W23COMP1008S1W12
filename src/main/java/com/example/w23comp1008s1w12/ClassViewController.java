5132b683eef5cd4901cea4b7c118ab98ce58c576
JaretWright <jaret.wright@georgiancollege.ca>
2023-04-13 17:17:18 -0400

    Week 13 - Updated view to accept a new Student object

--- src/main/java/com/example/w23comp1008s1w12/ClassViewController.java
+++ src/main/java/com/example/w23comp1008s1w12/ClassViewController.java
@@ -2,10 +2,14 @@ package com.example.w23comp1008s1w12;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.time.LocalDate;
@@ -13,6 +17,17 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class ClassViewController implements Initializable {
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private Label msgLabel;
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField studentNumTextField;
 
     @FXML
     private Label ageLabel;
@@ -28,6 +43,7 @@ public class ClassViewController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        msgLabel.setText("");
         ArrayList<Student> students = new ArrayList<>();
         Student homer = new Student("Homer",20023, LocalDate.of(1965,05,12));
         Student marge = new Student("Marge",20024, LocalDate.of(1977,04,12));
@@ -36,19 +52,35 @@ public class ClassViewController implements Initializable {
         students.add(marge);
         students.add(bart);
 
+        listView.getItems().add(homer);
+        listView.getItems().add(bart);
         listView.getItems().addAll(students);
+        listView.getItems().add(new Student("Maggie",20026,LocalDate.of(1990,01,29)));
+
 //        listView.getItems().add(homer.toString());
 //        listView.getItems().add(marge.toString());
 //        listView.getItems().add(bart.toString());
 
         //create a listener to observe when a user clicks on an item in the listview
-        listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, studentSelected) -> {
+        listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, studentSelected) ->
+        {
+            stNumLabel.setText("St Num: "+studentSelected.getStudentNum());
+            ageLabel.setText("Age: " + studentSelected.getAge());
             nameLabel.setText("Name: "+studentSelected.getName());
-            stNumLabel.setText("St #:"+studentSelected.getStudentNum());
-            ageLabel.setText("Age: " + studentSelected.getAge()+ " years");
         });
+    }
 
+    @FXML
+    void createStudent(ActionEvent event) {
+        String name = nameTextField.getText();
+        int studentNum = Integer.parseInt(studentNumTextField.getText());
+        LocalDate birthday = datePicker.getValue();
 
+        Student newStudent= new Student(name, studentNum,birthday);
+        listView.getItems().add(newStudent);
+    }
+    @FXML
+    void showName(MouseEvent event) {
 
     }
 }
